0d2584a02ea121e2a713bf118d1f4b39b1bf204a
Rida F'kih <hello@rida.dev>
2022-12-08 18:25:04 -0700

    add Middleware type as export

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 export { route } from "@/src/create-route";
 export { router } from "@/src/create-router";
 export { method } from "@/src/create-method";
+export type { Middleware } from "koa";
 
 export { z as validation } from "zod";
